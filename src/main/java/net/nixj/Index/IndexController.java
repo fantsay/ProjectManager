e305d841dcf68cffd6590ff26592ee63d3ed3bf0
Sergey <s6e8@localhost.localdomain>
2015-08-04 21:58:19 +0300

    Initial commit

---
+++ src/main/java/net/nixj/Index/IndexController.java
@@ -0,0 +1,9 @@
+package net.nixj.Index;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by s6e8 on 8/4/15.
+ */
+public class IndexController extends HttpServlet{
+}
---
+++ src/main/java/net/nixj/Login/LoginController.java
@@ -0,0 +1,19 @@
+package net.nixj.Login;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by s6e8 on 8/4/15.
+ */
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+}
